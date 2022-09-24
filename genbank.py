d7f43e0b62d813fc8ff7beec0c4cdad89697ef0b
Josue Figueroa <poor.rican@pm.me>
2022-09-24 07:53:29 -0700

    Updated typing and PEP errors

--- genbank.py
+++ genbank.py
@@ -2,7 +2,7 @@
 Methods to retrieve files from NCBI Genbank using Entrez
 """
 
-from typing import List
+from typing import List, TypeVar
 from Bio import Entrez, SeqIO
 from xml.etree import ElementTree
 
@@ -21,22 +21,23 @@ def find_in_genome(genome, term):
 
 
 # Get a list of reference genomes matching organism
-def get_genome_list(organism: str) -> List[SeqIO]:
+def get_genome_list(organism: str) -> List['SeqIO']:
     """
     Get RefSeq genomes from NCBI using a given query.
-    :param organism: search query
 
-    :returns List of `SeqRecord` objects containing reference genome
+    :param organism: search query to find organism on GenBank
+
+    :returns: List of `SeqRecord` objects containing reference genome
     """
     # TODO: have more expansive query
-    query = '"%s"[Organism] AND (Refseq[Filter] AND "bioproject nuccore"[Filter] AND "scope monoisolate"[Filter]) NOT plasmid[filter]' \
-            % organism
+    query = '"%s"[Organism] AND (Refseq[Filter] AND "bioproject nuccore"[Filter] AND "scope monoisolate"[Filter])' \
+            'NOT plasmid[filter]' % organism
     with Entrez.esearch(db='bioproject', term=query, retmode='text') as handle:
         records = Entrez.read(handle)
 
     accessions = []
-    for id in records['IdList']:
-        with Entrez.efetch(db='bioproject', id=id) as handle:
+    for uid in records['IdList']:
+        with Entrez.efetch(db='bioproject', id=uid) as handle:
             root = ElementTree.parse(handle).getroot()
             accession = root[0][0][0][0].attrib['accession']
             accessions.append(accession)
@@ -55,19 +56,23 @@ def get_genome_list(organism: str) -> List[SeqIO]:
 
 
 # Get genome data by id
-def fetch_genome(id: str) -> SeqIO:
-    with Entrez.efetch(db='nuccore', id=id, rettype='gb', retmode='text') as handle:
+def fetch_genome(uid: str) -> SeqIO:
+    with Entrez.efetch(db='nuccore', id=uid, rettype='gb', retmode='text') as handle:
         return SeqIO.read(handle, 'gb')
 
-def fetch_gene(id: str) -> SeqIO:
-    with Entrez.efetch(db='gene', id=id, rettype='gbwithparts', retmode='text') as handle:
+
+def fetch_gene(uid: str) -> SeqIO:
+    with Entrez.efetch(db='gene', id=uid, rettype='gbwithparts', retmode='text') as handle:
         return SeqIO.read(handle, 'gb')
 
 
 # Get genome data by accession number
-def fetch_genome_by_accession(accession: str) -> List[SeqIO]:
+def fetch_genome_by_accession(accession: str) -> List['SeqIO']:
     """
-    Search for genomes using accession number, then retrieve using id
+    Search for genomes using accession number, then retrieve using id.
+
+    This returns a list of `SeqIO` in the event that multiple genomes are returned, but they *should* not
+
     :param accession: Genome search term to use
 
     :returns list of `SeqIO` objects
@@ -81,33 +86,41 @@ def fetch_genome_by_accession(accession: str) -> List[SeqIO]:
     return genomes
 
 
-# Fetch all features found within a genome
+def get_all_genes(genome: SeqIO) -> List['SeqIO']:
+    """
+    Fetch all annotated genes in a genome from GenBank
 
-def get_all_genes(genome: SeqIO) -> List[SeqIO]:
+    :param genome: Annotated genomic `SeqIO` of interest
+
+    :return: List of annotated sequences
+    """
     # raise error if there are no annotations
     if len(genome.features) <= 1:
         raise ValueError('Genome does not have any gene annotations')
 
     # check if first feature is `source`
-    start = 0
-    if (genome.features[0].type == 'source'): start = 1
+    if genome.features[0].type == 'source':
+        start = 1
+    else:
+        start = 0
 
     genes = []
     # fetch all genes
-    for gene in genome.features:
-        gid = [i[i.index(':')+1:] for i in genomes[0].features[1].qualifiers['db_xref'] if 'GeneID' in i][0]
-        gene = fetch_gene(gid)
-        genes.append(gene)
+    for gene in genome.features[start:]:
+        gid = [i[i.index(':') + 1:] for i in gene.qualifiers['db_xref'] if 'GeneID' in i][0]
+        data = fetch_gene(gid)
+        genes.append(data)
     return genes
 
-def get_gene_metadata(id: str) -> dict['title': str, 'summary': str]:
+
+def get_gene_metadata(uid: str) -> dict[str, str]:
     """
     Get non-essential metadata not found in GenBank files by using defailt ASN.1 specification.
 
-    :param id: GeneID
+    :param uid: GeneID
     :return: dict containing `summary` and `title`
     """
-    with Entrez.efetch(db='gene', id=id, retmode='xml') as handle:
+    with Entrez.efetch(db='gene', id=uid, retmode='xml') as handle:
         parsed = ElementTree.parse(handle)
     root = parsed.getroot()
 
